1f1539ff20c3a08fc49c2b94ac5bea11a1d094ff
Charles Samonte <charles.deo024@hotmail.ca>
2023-06-17 11:31:35 -0600

    update

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,47 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let row of letters) {
+    let verticalString = '';
+    for (let col of row) {
+      verticalString += col;
     }
-}
+    if (verticalString.includes(word)) {
+      return true;
+    }
+  }
 
-module.exports = wordSearch
+  const numRows = letters.length;
+  const numCols = letters[0].length;
+
+  for (let col = 0; col < numCols; col++) {
+    let diagonalString = '';
+    let r = 0;
+    let c = col;
+    while (r < numRows && c < numCols) {
+      diagonalString += letters[r][c];
+      r++;
+      c++;
+    }
+    if (diagonalString.includes(word)) {
+      return true;
+    }
+  }
+
+  for (let row = 1; row < numRows; row++) {
+    let diagonalString = '';
+    let r = row;
+    let c = 0;
+    while (r < numRows && c < numCols) {
+      diagonalString += letters[r][c];
+      r++;
+      c++;
+    }
+    if (diagonalString.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
