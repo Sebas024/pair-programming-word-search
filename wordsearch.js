cc0c1b13f29bd6f39b867c9e579c6a5ec8d3fb15
Charles Samonte <charles.deo024@hotmail.ca>
2023-06-18 16:17:31 -0600

    Co-authored-by: KC Wayne <X33Ra@users.noreply.github.com>

--- wordsearch.js
+++ wordsearch.js
@@ -1,48 +1,28 @@
 const wordSearch = (letters, word) => {
-  const horizontalJoin = letters.map(ls => ls.join('')); // Join each inner array of letters horizontally into a string
+  // Convert each row of letters into a string
+  const horizontalJoin = letters.map(ls => ls.join(''));
 
-  for (let row of letters) { // Iterate over each row in the letters array
-    let verticalString = '';
-    for (let col of row) { // Iterate over each column in the current row
-      verticalString += col; // Build a string vertically by concatenating characters
+  // Iterate over each string in horizontalJoin
+  for (let l of horizontalJoin) {
+    // Check if the current string includes the word
+    if (l.includes(word)) {
+      return true; // Return true if the word is found horizontally
     }
-    if (verticalString.includes(word)) { // Check if the vertical string includes the target word
+  }
+
+  // Create the transposed array to enable vertical search
+  const transposedLetters = letters[0].map((col, i) => letters.map(row => row[i]));
+  const verticalJoin = transposedLetters.map(ls => ls.join(''));
+
+  // Iterate over each string in verticalJoin
+  for (let l of verticalJoin) {
+    // Check if the current string includes the word
+    if (l.includes(word)) {
       return true; // Return true if the word is found vertically
     }
   }
 
-  const numRows = letters.length; // Get the number of rows in the letters array
-  const numCols = letters[0].length; // Get the number of columns in the first row of the letters array
-
-  for (let col = 0; col < numCols; col++) { // Iterate over each column
-    let diagonalString = '';
-    let r = 0;
-    let c = col;
-    while (r < numRows && c < numCols) { // Traverse diagonally starting from the top-left corner
-      diagonalString += letters[r][c]; // Build a string by concatenating characters along the diagonal
-      r++;
-      c++;
-    }
-    if (diagonalString.includes(word)) { // Check if the diagonal string includes the target word
-      return true; // Return true if the word is found diagonally
-    }
-  }
-
-  for (let row = 1; row < numRows; row++) { // Iterate over each row starting from the second row
-    let diagonalString = '';
-    let r = row;
-    let c = 0;
-    while (r < numRows && c < numCols) { // Traverse diagonally starting from the top-left corner of each row
-      diagonalString += letters[r][c]; // Build a string by concatenating characters along the diagonal
-      r++;
-      c++;
-    }
-    if (diagonalString.includes(word)) { // Check if the diagonal string includes the target word
-      return true; // Return true if the word is found diagonally
-    }
-  }
-
-  return false; // Return false if the word is not found in any direction
+  return false; // Return false if the word is not found
 };
 
-module.exports = wordSearch; // Export the wordSearch function as the module's export
+module.exports = wordSearch;
